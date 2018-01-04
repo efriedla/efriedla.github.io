9c66df92f70cecf286cf082167becba8fec8fea3
efriedla <efriedla20852@gmail.com>
2018-01-04 12:39:01 -0800

    resume

--- js/addOns.js
+++ js/addOns.js
@@ -1,24 +1,18 @@
 $(document).ready(function(){
-	
+	//Portfolio Tabs
+	// on tab click
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
-
+		//shows content on current tab
 		$('ul.tabs li').removeClass('current');
 		$('.tab-content').removeClass('current');
-
+		//add class to be shown
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
 	});
 
-
 	// show email
-	// function showEmail() {
-	// 	alter("clicked");
-	// 	// $("#myEmail").innerHTML = "Hello World";
-	// }
 	$("#showEmail").click(function(){
-		
 		$("#myEmail").text("efriedla20852@gmail.com");
     });
-
 });
